26b3acc00440903fef9dd114eac23d8ee2d84f1c
Redrik <79138016+redrik05@users.noreply.github.com>
2024-09-18 02:54:44 +0300

    Delete PP_3_1_2_Boot_Security-master55 directory

--- PP_3_1_2_Boot_Security-master55/src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.models.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role getRoleByName(String name);
-}
